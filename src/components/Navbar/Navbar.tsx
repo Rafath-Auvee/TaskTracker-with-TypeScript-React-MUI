f9ac588ac697aa856a4b9b10b48dfd0c4dce2521
Rafath Bin Zafar Auvee <61088535+Rafath-Bin-Zafar-Auvee@users.noreply.github.com>
2022-07-09 11:00:22 +0600

    Navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,19 +1,35 @@
 import React from "react";
-import { AppBar, Toolbar, Typography, styled } from "@mui/material";
+import { AppBar, Toolbar, Typography, styled} from "@mui/material";
+import { Box } from '@mui/system';
+import TaskAltIcon from "@mui/icons-material/TaskAlt";
 
 const StyleNavbar = styled(Toolbar)({
   display: "flex",
   justifyContent: "space-between",
 });
 
+const Icons = styled(Box)({
+  display: "flex",
+  alignItems: "center",
+  gap: "20px",
+});
+
 const Navbar = () => {
   return (
     <AppBar position="sticky">
       <StyleNavbar>
-        <Typography variant="h6" color="">
-          Task Track
+        <Typography
+          variant="h6"
+          color=""
+          sx={{ display: { xs: "none", sm: "block" } }}
+        >
+          Task Tracker
         </Typography>
+        <TaskAltIcon sx={{ display: { xs: "block", sm: "none" } }} />
       </StyleNavbar>
+      <Icons>
+
+      </Icons>
     </AppBar>
   );
 };
