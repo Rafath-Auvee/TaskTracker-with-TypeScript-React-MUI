324f9a0534c82e1bed251ed8bc7c449ea0fec2bc
Rafath Bin Zafar Auvee <61088535+Rafath-Bin-Zafar-Auvee@users.noreply.github.com>
2022-07-09 11:32:37 +0600

    navbar complete

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,10 +1,22 @@
 import React from "react";
-import { AppBar, Toolbar, Typography, styled} from "@mui/material";
-import { Box } from '@mui/system';
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  styled,
+  Box,
+  Badge,
+  Stack,
+} from "@mui/material";
+
 import TaskAltIcon from "@mui/icons-material/TaskAlt";
+import AssignmentIcon from "@mui/icons-material/Assignment";
+
+import {  Mail, Notifications, Pets } from "@mui/icons-material";
 
 const StyleNavbar = styled(Toolbar)({
   display: "flex",
+  // flex-direction: "column",
   justifyContent: "space-between",
 });
 
@@ -17,19 +29,29 @@ const Icons = styled(Box)({
 const Navbar = () => {
   return (
     <AppBar position="sticky">
-      <StyleNavbar>
-        <Typography
-          variant="h6"
-          color=""
-          sx={{ display: { xs: "none", sm: "block" } }}
-        >
-          Task Tracker
-        </Typography>
-        <TaskAltIcon sx={{ display: { xs: "block", sm: "none" } }} />
-      </StyleNavbar>
-      <Icons>
-
-      </Icons>
+      <Stack
+        direction="row"
+        justifyContent="space-between"
+        alignItems="center"
+        
+        // spacing={12}
+      >
+        <StyleNavbar>
+          <Typography
+            variant="h6"
+            color=""
+            sx={{ display: { xs: "none", sm: "block" } }}
+          >
+            Task Tracker
+          </Typography>
+          <TaskAltIcon sx={{ display: { xs: "block", sm: "none" } }} />
+        </StyleNavbar>
+        <Icons>
+          <Badge badgeContent={4} color="error" sx={{ mr: "2rem" }}>
+            <AssignmentIcon />
+          </Badge>
+        </Icons>
+      </Stack>
     </AppBar>
   );
 };
