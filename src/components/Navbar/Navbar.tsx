3a6b34683d25b1be29674d372d9eea4fa6e8bb8e
Rafath Bin Zafar Auvee <61088535+Rafath-Bin-Zafar-Auvee@users.noreply.github.com>
2022-07-09 09:53:08 +0600

    Basic Navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,12 +1,21 @@
-import React from 'react'
-import { AppBar } from '@mui/material';
+import React from "react";
+import { AppBar, Toolbar, Typography, styled } from "@mui/material";
+
+const StyleNavbar = styled(Toolbar)({
+  display: "flex",
+  justifyContent: "space-between",
+});
+
 const Navbar = () => {
-
   return (
-    <AppBar>
-      Hello
+    <AppBar position="sticky">
+      <StyleNavbar>
+        <Typography variant="h6" color="">
+          Task Track
+        </Typography>
+      </StyleNavbar>
     </AppBar>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
